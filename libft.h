ab9df6be4fc4330d1d2f558531c50ac8aa9db861
William Asman <wasman@Bocals-iMac-19.local>
2016-09-23 10:21:30 -0700

    fixed norm

--- libft.h
+++ libft.h
@@ -6,16 +6,16 @@
 /*   By: wasman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 09:25:16 by wasman            #+#    #+#             */
-/*   Updated: 2016/09/22 20:59:51 by wasman           ###   ########.fr       */
+/*   Updated: 2016/09/23 10:11:09 by wasman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define	LIBFT_H
+# define LIBFT_H
 
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
